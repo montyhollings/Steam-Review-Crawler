4eee6f59c73aa5ce7c9158a8e210ebf35a872bb3
Monty Hollings <hollings@protonmail.com>
2021-04-20 18:50:58 +0100

    Introduced a basic ReviewCrawler class. Initially just going for one batch of 100 reviews and logging them to the console.

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import requests
 import json
 
-app_id = 1382330
-reviews_response = requests.get(f'https://store.steampowered.com/appreviews/{app_id}?json=1')
-print(reviews_response.json())
+class ReviewCrawler:
+    def __init__(self, gamename, franchise):
+        self.gameName = gamename
+        self.franchise = franchise
+        self.reviews = {}
+        self.source = "steam"
+        self.appId = 1382330
+
+    def get_reviews(self):
+        reviews_response = requests.get(f'https://store.steampowered.com/appreviews/{self.appId}?json=1&num_per_page=100')
+        json_response = reviews_response.json()
+        self.reviews = json_response['reviews']
+        self.display_reviews()
+
+    def display_reviews(self):
+        for y in range(100):
+            print(self.reviews[y])
+
+
+
+game_name = "Persona 5"
+franchise = "Persona 5"
+NewCrawler = ReviewCrawler(game_name, franchise)
+NewCrawler.get_reviews()
