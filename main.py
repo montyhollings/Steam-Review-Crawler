d706e82f5bc2266abd2ac7bd8588775049a27268
Monty Hollings <hollings@protonmail.com>
2021-04-22 22:09:18 +0100

    trying a new way of approach

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import json
 import math
 import urllib.parse
 import copy
-
-
+import os
+from slugify import slugify
+print(os.getcwd())
 class ReviewCrawler:
     def __init__(self, gamename, franchise, appid=1382330):
         # Hard code source as per spec,
@@ -21,15 +22,18 @@ class ReviewCrawler:
         self.cursor = "*"
         self.source = "steam"
         self.appId = appid
+        self.output_dir = os.getcwd()
 
-    def setup_parameters(self, language, date, per_page):
+    def setup_parameters(self, language="english", date="3650", per_page="100", review_type="all", purchase_type="all"):
         # Setup parameters for the requests
         self.parameters = {
             "filter": "all",
             "language": language,
             "day_range": date,
             "num_per_page": per_page,
-            "cursor": self.cursor
+            "cursor": self.cursor,
+            "review_type": review_type,
+            "purchase_type": purchase_type,
         }
         # Get the total review count so we know the loop indexes
         self.get_total_review_count()
@@ -45,11 +49,29 @@ class ReviewCrawler:
         # Divide that number by the amount per page and round up the result to get the number of batches needed
         self.total_review_count = response.json()['query_summary']['total_reviews']
         self.total_review_batches = math.ceil(self.total_review_count / self.parameters['num_per_page'])
-        self.review_batch_loop()
 
-    def review_batch_loop(self):
+    def get_reviews(self):
+        print(self.total_review_count)
+        for i in range(math.floor(self.total_review_count / 100)):
+            print(i)
+            output_file_path = self.get_filepath()
+            if os.path.exists(output_file_path):
+                self.cursor = self.get_cursor_from_file()
+                continue
+            else:
+                print('ERROR')
+            response = self.send_request()
+            if response.status_code == 200:
+                json_response = response.json()
+                self.cursor = urllib.parse.quote(json_response['cursor'])
+                with open(output_file_path, "w") as f:
+                    json.dump(fp=f, obj=json_response, indent=4)
+            else:
+                print('ERROR')
+                # self.dump_to_file(json_response, output_file_path)
         # Loop over the amount of review batches
-        for index in range(self.total_review_batches):
+        while self.total_review_count > 0:
+
             # Send request
             request_response = self.send_request()
             # Format request and save properties
@@ -58,20 +80,10 @@ class ReviewCrawler:
             self.number_in_batch = json_response['query_summary']['num_reviews']
             self.batch_reviews = json_response['reviews']
             # For each batch of reviews, we need to process and format them
-            self.process_reviews()
 
     def send_request(self):
-        response = requests.get(
-            f'https://store.steampowered.com/appreviews/{self.appId}?json=1&num_per_page=100', params=self.parameters)
-        return response
-
-    def process_reviews(self):
-        # Loop over every review (always 100 per batch)
-        # send json to be formatted along with its index which I shall use as the ID for now
-        for counter in range(self.number_in_batch):
-            self.formatted_reviews[self.review_counter] = self.format_review(self.batch_reviews[counter],
-                                                                             self.review_counter)
-            self.review_counter += 1
+        return requests.get(
+            f'https://store.steampowered.com/appreviews/{self.appId}?json=1&', params=self.parameters)
 
     def format_review(self, review_instance, review_id):
         # Return review in desired format
@@ -79,6 +91,7 @@ class ReviewCrawler:
         # TODO: finish proper UUID for each review
         return {
             'id': review_id,
+            'review_counter': self.total_review_count,
             'author': hash(review_instance['author']['steamid']),
             'date': review_instance['timestamp_created'],
             'hours': review_instance['author']['playtime_at_review'],
@@ -97,6 +110,24 @@ class ReviewCrawler:
         with open('data.json', 'w') as outfile:
             json.dump(self.formatted_reviews, outfile, indent=4)
 
+    def dump_to_file(self, json_request, file_path):
+        with open(file_path, "w") as path:
+            json.dump(path, json_request)
+
+    def get_filepath(self):
+        return os.path.join(
+            self.output_dir,
+            f"BATCH-{slugify(self.cursor)}.json"
+        )
+
+    def get_cursor_from_file(self):
+
+        find_cursor_in = self.get_filepath()
+        with open(find_cursor_in, "r") as f:
+            v = json.load(f)
+            cursor = urllib.parse.quote(v['cursor'])
+            return cursor
+
 
 # Hard core game name & franchise for now to something temporary
 game_name = "Persona 5"
@@ -112,4 +143,4 @@ num_per_page = 100
 # Create new crawler instance
 # Display reviews
 NewCrawler.setup_parameters(language, day_range, num_per_page)
-NewCrawler.display_reviews()
+NewCrawler.get_reviews()
