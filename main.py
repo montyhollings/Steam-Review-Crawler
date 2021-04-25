5001d73faaf41cee5cdf0e695c3c8b0a1c5b22c3
Monty Hollings <montyhollings@mailbox.org>
2021-04-25 22:44:13 +0100

    Still stuck, going to try a different approach next

--- main.py
+++ main.py
@@ -5,44 +5,73 @@ import urllib.parse
 import copy
 import os
 from slugify import slugify
+import datetime
 import time
 
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
 
-def initialise_crawler(game_name, franchise):
+    However pay attention at the formatting used in
+    this function because it is programmed to be pretty
+    printed and may differ from the actual request.
+    """
+    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
+
+def setup_date_filter():
+    date_valid = False
+    while date_valid == False:
+        filter_from = input('Please enter the date you wish to filter from in format [yyyy-mm-dd]: ')
+        try:
+            from_date = datetime.datetime.strptime(filter_from, '%Y-%m-%d')
+            date_valid = True
+        except ValueError:
+            print('Incorrect please try again, e.g 2001-01-01')
+    todays_date = datetime.datetime.now()
+    print((todays_date - from_date).days)
+    return (todays_date - from_date).days
+
+
+def initialise_crawler(game_name="Persona 5", franchise="Persona 5", filter_range=30000):
     print('Starting Crawler...')
-    NewCrawler = ReviewCrawler(game_name, franchise)
+    NewCrawler = ReviewCrawler(game_name, franchise, filter_range)
     print('Setting up crawler configuration...')
     NewCrawler.setup_parameters()
     print('Crawling...')
-    NewCrawler.get_reviews()
+    # NewCrawler.get_reviews()
 
 
 class ReviewCrawler:
-    def __init__(self, gamename, franchise, appid=1382330):
+    def __init__(self, gamename, franchise, filter_range, appid=1382330):
         # Hard code source as per spec,
         # Not sure if you should be able to change appId by input etc
         self.gameName = gamename
         self.franchise = franchise
+        self.filter_range = filter_range
         self.total_review_count = 0
         self.request_loop_count = 0
         self.file_count = 0
         self.review_counter = 0
         self.formatted_reviews = {}
         self.parameters = {}
-        self.batch_reviews = {}
         self.reviews = {}
         self.number_in_batch = 0
         self.cursor = "*"
         self.source = "steam"
         self.appId = appid
-        self.output_dir = os.getcwd()
 
-    def setup_parameters(self, date="3650", language="english", per_page="100", review_type="all", purchase_type="all"):
+    def setup_parameters(self, language="english", per_page="100", review_type="all", purchase_type="all"):
         # Setup parameters for the requests
         self.parameters = {
             "filter": "all",
             "language": language,
-            "day_range": date,
+            "day_range": self.filter_range,
             "cursor": self.cursor,
             "review_type": review_type,
             "purchase_type": purchase_type,
@@ -55,45 +84,49 @@ class ReviewCrawler:
         # TODO: accept date parameters
         # Overwrite the num per page to 1, this will save speed by making the response much smaller
         temp_parameters = copy.copy(self.parameters)
+        print(self.parameters)
         temp_parameters['num_per_page'] = "1"
-        response = requests.get(
-            f'https://store.steampowered.com/appreviews/{self.appId}?json=1', params=temp_parameters)
+        response = requests.Request("GET",
+            f'https://store.steampowered.com/appreviews/{self.appId}?json=1', data=temp_parameters)
+        prepared = response.prepare()
+        pretty_print_POST(prepared)
         # Grab the total number of reviews in the response
         # Divide that number by the amount per page and round up the result to get the number of batches needed
-        self.total_review_count = int(response.json()['query_summary']['total_reviews'])
-        self.request_loop_count = math.floor(self.total_review_count / 100)
-        self.file_count = math.ceil(self.total_review_count / 5000)
-        print('total reviews: ' + str(self.total_review_count))
+        # self.total_review_count = int(response.json()['query_summary']['total_reviews'])
+        # self.request_loop_count = math.floor(self.total_review_count / 100)
+        # self.file_count = math.ceil(self.total_review_count / 200)
+        # print('total reviews: ' + str(self.total_review_count))
 
     def get_reviews(self):
-        for x in range(self.file_count):
-            for i in range(self.request_loop_count):
-                print('------ LOOP ------ ')
-                print('index: ' + str(self.review_counter))
-                print('old_cursor: ' + str(self.parameters['cursor']))
-                print('pre-add reviews=: ' + str(self.reviews.keys()))
-                if i == 0:
-                    slug = "*"
-                else:
-                    slug = slugify(self.parameters['cursor'])
 
-                if slug in self.reviews:
-                    print('cursor: ' + str(self.parameters['cursor'] + ' is a dupe'))
-                    self.parameters['cursor'] = self.reviews[slug]['cursor']
-                    continue
+        for i in range(self.request_loop_count):
+            print('------ LOOP ------ ')
+            print('index: ' + str(self.review_counter))
+            print('old_cursor: ' + str(self.parameters['cursor']))
+            print('pre-add reviews=: ' + str(self.reviews.keys()))
+            if i == 0:
+                slug = "*"
+            else:
+                slug = slugify(self.parameters['cursor'])
 
-                response = self.send_request()
+            if slug in self.reviews:
+                print('cursor: ' + str(self.parameters['cursor'] + ' is a dupe'))
+                self.parameters['cursor'] = str(urllib.parse.quote(self.reviews[slug]['cursor']))
+                continue
 
-                json_response = response.json()
-                print('raw_cursor: ' + str(json_response['cursor']))
-                self.parameters['cursor'] = json_response['cursor']
-                print('encoded_cursor: ' + str(self.parameters['cursor']))
-                print('slug: ' + str(slug))
-                self.reviews[slug] = json_response
-                print('adding: ' + str(slug) + ' to self.reviews')
-                self.review_counter += 1
-                self.request_loop_count -= 1
-                print(self.request_loop_count)
+            response = self.send_request()
+
+            json_response = response.json()
+            print('raw_cursor: ' + str(json_response['cursor']))
+            self.parameters['cursor'] = str(urllib.parse.quote(json_response['cursor']))
+            print('encoded_cursor: ' + str(self.parameters['cursor']))
+            print('slug: ' + str(slug))
+            self.reviews[slug] = json_response
+            print('adding: ' + str(slug) + ' to self.reviews')
+            self.review_counter += 1
+            self.request_loop_count -= 1
+            print(self.request_loop_count)
+            time.sleep(1.5)
 
         # LOOP ENDS ALL REVIEWS ADDED TO SELF.REVIEWS
         for key, value in self.reviews.items():
@@ -134,7 +167,12 @@ class ReviewCrawler:
 
 
 # Hard core game name & franchise for now to something temporary
-game_name = "Persona 5"
-franchise = "Persona 5"
+game_name = input('Please enter the name of the game you are crawling: ')
+franchise = input('Please enter the name of the game you are crawling: ')
+filter_by_date = input('If you wish to enter by date, please enter Y or y, else hit enter: ')
+if(filter_by_date == "Y" or filter_by_date == "y"):
+    date_range = setup_date_filter()
+else:
+    date_range = None
 
-initialise_crawler(game_name, franchise)
+initialise_crawler(game_name, franchise, date_range)
