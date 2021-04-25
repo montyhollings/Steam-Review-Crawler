7e3d796e5cab05fc625846e31def200f9263ba49
Monty Hollings <hollings@protonmail.com>
2021-04-25 12:30:07 +0100

    changed from cursor per file to storing it in internal memory

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import urllib.parse
 import copy
 import os
 from slugify import slugify
-print(os.getcwd())
+import time
+
+
 class ReviewCrawler:
     def __init__(self, gamename, franchise, appid=1382330):
         # Hard code source as per spec,
@@ -13,11 +15,12 @@ class ReviewCrawler:
         self.gameName = gamename
         self.franchise = franchise
         self.total_review_count = 0
-        self.total_review_batches = 0
+        self.request_loop_count = 0
         self.review_counter = 0
         self.formatted_reviews = {}
         self.parameters = {}
         self.batch_reviews = {}
+        self.reviews = {}
         self.number_in_batch = 0
         self.cursor = "*"
         self.source = "steam"
@@ -30,10 +33,10 @@ class ReviewCrawler:
             "filter": "all",
             "language": language,
             "day_range": date,
-            "num_per_page": per_page,
             "cursor": self.cursor,
             "review_type": review_type,
             "purchase_type": purchase_type,
+            "num_per_page": per_page,
         }
         # Get the total review count so we know the loop indexes
         self.get_total_review_count()
@@ -47,51 +50,55 @@ class ReviewCrawler:
             f'https://store.steampowered.com/appreviews/{self.appId}?json=1', params=temp_parameters)
         # Grab the total number of reviews in the response
         # Divide that number by the amount per page and round up the result to get the number of batches needed
-        self.total_review_count = response.json()['query_summary']['total_reviews']
-        self.total_review_batches = math.ceil(self.total_review_count / self.parameters['num_per_page'])
+        self.total_review_count = int(response.json()['query_summary']['total_reviews'])
+        self.request_loop_count = math.floor(self.total_review_count / 100)
+        print('total reviews: ' + str(self.total_review_count))
 
     def get_reviews(self):
-        print(self.total_review_count)
-        for i in range(math.floor(self.total_review_count / 100)):
-            print(i)
-            output_file_path = self.get_filepath()
-            if os.path.exists(output_file_path):
-                self.cursor = self.get_cursor_from_file()
-                continue
-            else:
-                print('ERROR')
-            response = self.send_request()
-            if response.status_code == 200:
-                json_response = response.json()
-                self.cursor = urllib.parse.quote(json_response['cursor'])
-                with open(output_file_path, "w") as f:
-                    json.dump(fp=f, obj=json_response, indent=4)
-            else:
-                print('ERROR')
-                # self.dump_to_file(json_response, output_file_path)
-        # Loop over the amount of review batches
-        while self.total_review_count > 0:
 
-            # Send request
-            request_response = self.send_request()
-            # Format request and save properties
-            json_response = request_response.json()
-            self.parameters['cursor'] = json_response['cursor']
-            self.number_in_batch = json_response['query_summary']['num_reviews']
-            self.batch_reviews = json_response['reviews']
-            # For each batch of reviews, we need to process and format them
+        counter = 0
+        for i in range(self.request_loop_count):
+            print('------ LOOP ------ ')
+            print('index: ' + str(counter))
+            print('old_cursor: ' + str(self.parameters['cursor']))
+            if i == 0:
+                slug = "*"
+            else:
+                counter += 1
+                slug = slugify(self.parameters['cursor'])
+
+            if slug in self.reviews:
+                self.parameters['cursor'] = urllib.parse.quote_plus(self.reviews[slug]['cursor'])
+                continue
+
+            response = self.send_request()
+
+            json_response = response.json()
+            print('raw_cursor: ' + str(json_response['cursor']))
+            response_slug = slugify(json_response['cursor'])
+            self.parameters['cursor'] = urllib.parse.quote_plus(json_response['cursor'])
+            print('encoded_cursor: ' + str(self.parameters['cursor']))
+            self.reviews[response_slug] = json_response
+            print('------------')
+
+            # LOOP ENDS ALL REVIEWS ADDED TO SELF.REVIEWS
+        for key, value in self.reviews.items():
+            for x in value['reviews']:
+                self.formatted_reviews[self.review_counter] = self.format_review(x)
+                self.review_counter += 1
+
+        self.display_reviews()
 
     def send_request(self):
         return requests.get(
             f'https://store.steampowered.com/appreviews/{self.appId}?json=1&', params=self.parameters)
 
-    def format_review(self, review_instance, review_id):
+    def format_review(self, review_instance):
         # Return review in desired format
         # TODO: finish author hash instead of current hack
         # TODO: finish proper UUID for each review
         return {
-            'id': review_id,
-            'review_counter': self.total_review_count,
+            'id': self.review_counter,
             'author': hash(review_instance['author']['steamid']),
             'date': review_instance['timestamp_created'],
             'hours': review_instance['author']['playtime_at_review'],
@@ -110,23 +117,7 @@ class ReviewCrawler:
         with open('data.json', 'w') as outfile:
             json.dump(self.formatted_reviews, outfile, indent=4)
 
-    def dump_to_file(self, json_request, file_path):
-        with open(file_path, "w") as path:
-            json.dump(path, json_request)
 
-    def get_filepath(self):
-        return os.path.join(
-            self.output_dir,
-            f"BATCH-{slugify(self.cursor)}.json"
-        )
-
-    def get_cursor_from_file(self):
-
-        find_cursor_in = self.get_filepath()
-        with open(find_cursor_in, "r") as f:
-            v = json.load(f)
-            cursor = urllib.parse.quote(v['cursor'])
-            return cursor
 
 
 # Hard core game name & franchise for now to something temporary
